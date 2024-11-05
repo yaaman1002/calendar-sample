03ea190cba7e4787567bad86713faccb2fdff6d6
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-11-05 13:58:43 +0000

    [add]カレンダー機能

---
+++ app/javascript/packs/calendar.js
@@ -0,0 +1,14 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    events: '/events'
+  });
+
+  calendar.render();
+});
